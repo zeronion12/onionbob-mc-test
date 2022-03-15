d17bf9db9722b94a045fe7e032412c3ecd3d222d
NoName <nonamestar00@gmail.com>
2022-03-15 15:36:50 +0800

    edit chart

--- server.js
+++ server.js
@@ -1,49 +1,50 @@
+// Now make both Main chat function and events chat function the same
+// Now test 
 var mineflayer = require('mineflayer');
-var pass = "12345"; //Authme plugini şifresi (Plugin yoksa silmeyin)
+var pass = "12345"; // "Authme" plugin password (No plugin No delete)
+
+// Configurations, Could all be "process.env.XXX" and set in Heroku
 var ayar = {
-  host: "ult12.falix.gg", //Sunucunuzun ipsi
-  port: process.env.port || 48481, //Değiştirme!!!
-  username: "ONIONBOT2", //Botun ismi Kafanıza Göre
-  version: "1.18.1" //Değiştirme!!!
-  
+  host: "ult12.falix.gg", // Server IP
+  port: process.env.port || 48481, // Need change to target port
+  username: "ONIONBOT2", // Bot Name in Minecraft
+  version: "1.18.1" // Need change to target version
 };
 
+// ---Main Program--- //
 var bot = mineflayer.createBot(ayar);
-
-
-bot.on('chat', function(username, message) {
-  if (username === bot.username) return;
-  function intervalFunc() {
-    bot.setControlState('forward', true)
-  }
-  setInterval(intervalFunc,7000);
-  console.log('Interval Message!');
-  bot.chat('/login '+ pass);
-});
-
 bindEvents(bot);
+// ------------------ //
+
+// Events to bind
 function bindEvents(bot) {
   
+  // Continuous function to pretend Bot running (60 min interval to set control state)
+  bot.on('chat', function(username, message) {
+    if (username === bot.username) return;
+    function intervalFunc() {
+      bot.setControlState('forward', true)
+    }
+    setInterval(intervalFunc,60000);
+    console.log('Interval Message!');
+    bot.chat('/login '+ pass);
+  });
+
+  // Error function
   bot.on('error', function(err) {
     console.log("Error!");
   });
   
+  // Exception function: if End => Relogin
   bot.on('end', function() {
     console.log("End!");
     setTimeout(relog, 5000);
   });
   
+  // Relogin function: do the same like Main Program
   function relog() {
     console.log("Relogin!");
     bot = mineflayer.createBot(ayar);
-    
-    bot.on('chat', function(username, message) {
-      if (username === bot.username) return;
-      console.log("Relogin Success!");
-      bot.chat('/login '+ pass);
-});
-    
-    
     bindEvents(bot);
   }
 }
